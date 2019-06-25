65e8eeb68e9f8450b2dcfbdb60723b6b20e2cced
willcl-ark <will8clark@gmail.com>
2019-06-25 21:52:51 +0100

    Move non order-specific methods outside Class
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- rest_api.py
+++ rest_api.py
@@ -21,6 +21,9 @@ TESTNET_SATELLITE_API = "https://api.blockstream.space/testnet"
 #
 #     return handle_response_wrapper
 
+def handle_response(response):
+    return json.loads(response.text)
+
 
 class Order:
 
@@ -43,22 +46,18 @@ class Order:
         elif self.network == 'testnet':
             self.satellite_url = TESTNET_SATELLITE_API
 
-    @staticmethod
-    def handle_response(response):
-        return json.loads(response.text)
-
     def place(self, bid):
         """Place an order for a message transmission.
 
-            If successful, the response includes the JSON Lightning invoice as returned by Lightning
-            Charge's POST /invoice and an authentication token that can be used to modify the order.
-            """
+        If successful, the response includes the JSON Lightning invoice as returned by Lightning
+        Charge's POST /invoice and an authentication token that can be used to modify the order.
+        """
         data = {"message": self.message,
                 "bid": bid}
         response = requests.post(url=f"{self.satellite_url}/order", data=data)
         self.api_status_code = response.status_code
         if response.status_code == 200:
-            self.place_order = self.handle_response(response)
+            self.place_order = handle_response(response)
             self.auth_token = self.place_order['auth_token']
             self.uuid = self.place_order['uuid']
             return
@@ -75,7 +74,7 @@ class Order:
         response = requests.post(url=f"{self.satellite_url}/order/{self.uuid}/bump", data=data)
         self.api_status_code = response.status_code
         if response.status_code == 200:
-            self.bump_order = self.handle_response(response)
+            self.bump_order = handle_response(response)
             return
         return response.status_code, response.reason, response.text
 
@@ -86,7 +85,7 @@ class Order:
         response = requests.get(url=f"{self.satellite_url}/order/{self.uuid}", headers=headers)
         self.api_status_code = response.status_code
         if response.status_code == 200:
-            self.get_order = self.handle_response(response)
+            self.get_order = handle_response(response)
             return
         return response.status_code, response.reason, response.text
 
@@ -97,59 +96,59 @@ class Order:
         response = requests.delete(url=f"{self.satellite_url}/order/{self.uuid}", data=data)
         self.api_status_code = response.status_code
         if response.status_code == 200:
-            self.delete_order = self.handle_response(response)
+            self.delete_order = handle_response(response)
             return
         return response.status_code, response.reason, response.text
 
-    @classmethod
-    def pending(cls, sat_url, before_iso8601=None):
-        """Retrieve a list of 20 orders awaiting payment ordered by creation time.
 
-        For pagination, optionally specify a before parameter (in ISO 8601 format) that specifies
-        that the 20 orders immediately prior to the given time be returned.
-        """
-        if before_iso8601:
-            response = requests.get(url=f"{sat_url}/orders/pending?before={before_iso8601}")
-        else:
-            response = requests.get(url=f"{sat_url}/orders/pending")
-        if response.status_code == 200:
-            return cls.handle_response(response)
-        return response.status_code, response.reason, response.text
+def pending(sat_url, before_iso8601=None):
+    """Retrieve a list of 20 orders awaiting payment ordered by creation time.
 
-    @classmethod
-    def queued(cls, sat_url, limit=None):
-        """Retrieve a list of paid, but unsent orders in descending order of bid-per-byte.
-        Both pending orders and the order currently being transmitted are returned.
-        Optionally, accepts a parameter specifying how many queued order to return.
-        """
-        if limit:
-            response = requests.get(url=f"{sat_url}/orders/queued?limit={limit}")
-        else:
-            response = requests.get(url=f"{sat_url}/orders/queued")
-        if response.status_code == 200:
-            return cls.handle_response(response)
-        return response.status_code, response.reason, response.text
+    For pagination, optionally specify a before parameter (in ISO 8601 format) that specifies
+    that the 20 orders immediately prior to the given time be returned.
+    """
+    if before_iso8601:
+        response = requests.get(url=f"{sat_url}/orders/pending?before={before_iso8601}")
+    else:
+        response = requests.get(url=f"{sat_url}/orders/pending")
+    if response.status_code == 200:
+        return handle_response(response)
+    return response.status_code, response.reason, response.text
 
-    @classmethod
-    def sent(cls, sat_url, before_iso8601=None):
-        """Retrieves a list of up to 20 sent orders in reverse chronological order.
-        For pagination, optionally specify a before parameter (in ISO 8601 format) that specifies
-        that the 20 orders immediately prior to the given time be returned.
-        """
-        if before_iso8601:
-            response = requests.get(url=f"{sat_url}/orders/sent?before={before_iso8601}")
-        else:
-            response = requests.get(url=f"{sat_url}/orders/sent")
-        if response.status_code == 200:
-            return cls.handle_response(response)
-        return response.status_code, response.reason, response.text
 
-    @classmethod
-    def sat_ln_node_info(cls, sat_url):
-        """Returns information about the c-lightning node where satellite API payments are
-        terminated.
-        """
-        response = requests.get(url=f"{sat_url}/info")
-        if response.status_code == 200:
-            return cls.handle_response(response)
-        return response
+def queued(sat_url, limit=None):
+    """Retrieve a list of paid, but unsent orders in descending order of bid-per-byte.
+    Both pending orders and the order currently being transmitted are returned.
+    Optionally, accepts a parameter specifying how many queued order to return.
+    """
+    if limit:
+        response = requests.get(url=f"{sat_url}/orders/queued?limit={limit}")
+    else:
+        response = requests.get(url=f"{sat_url}/orders/queued")
+    if response.status_code == 200:
+        return handle_response(response)
+    return response.status_code, response.reason, response.text
+
+
+def sent_orders(sat_url, before_iso8601=None):
+    """Retrieves a list of up to 20 sent orders in reverse chronological order.
+    For pagination, optionally specify a before parameter (in ISO 8601 format) that specifies
+    that the 20 orders immediately prior to the given time be returned.
+    """
+    if before_iso8601:
+        response = requests.get(url=f"{sat_url}/orders/sent?before={before_iso8601}")
+    else:
+        response = requests.get(url=f"{sat_url}/orders/sent")
+    if response.status_code == 200:
+        return handle_response(response)
+    return response.status_code, response.reason, response.text
+
+
+def sat_ln_node_info(sat_url):
+    """Returns information about the c-lightning node where satellite API payments are
+    terminated.
+    """
+    response = requests.get(url=f"{sat_url}/info")
+    if response.status_code == 200:
+        return handle_response(response)
+    return response
