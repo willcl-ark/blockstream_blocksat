c0a71d2b53a9a9cfd41e9131153cb6f7858bcf89
willcl-ark <will8clark@gmail.com>
2019-07-16 10:57:35 +0100

    Refactored to work with API
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- blocksat_api/blocksat.py
+++ blocksat_api/blocksat.py
@@ -1,138 +1,81 @@
-import json
-from sys import getsizeof
-
 import requests
 
 SATELLITE_API = "https://api.blockstream.space"
 TESTNET_SATELLITE_API = "https://api.blockstream.space/testnet"
 
 
-def handle_response(response):
-    return json.loads(response.text)
+def place(message, bid, satellite_url):
+    """Place an order for a message transmission.
+
+    If successful, the response includes the JSON Lightning invoice as returned by Lightning
+    Charge's POST /invoice and an authentication token that can be used to modify the order.
+    """
+    data = {"message": message,
+            "bid": bid}
+    return requests.post(url=f"{satellite_url}/order", data=data)
 
 
-class Order:
+def bump_order(uuid, auth_token, bid_increase, satellite_url):
+    """Increase the bid for an order sitting in the transmission queue.
 
-    def __init__(self, auth_token=None, message=None, network='mainnet', uuid=None):
-        self.auth_token = auth_token
-        self.api_status_code = None
-        self.message = message
-        self.size = len(self.message.encode('utf-8'))
-        self.network = network
-        self.bump_response = None
-        self.delete_response = None
-        self.get_response = None
-        self.place_response = None
-        self.satellite_url = None
-        self.uuid = uuid
-        if self.network == 'mainnet':
-            self.satellite_url = SATELLITE_API
-        elif self.network == 'testnet':
-            self.satellite_url = TESTNET_SATELLITE_API
-
-    def place(self, bid):
-        """Place an order for a message transmission.
-
-        If successful, the response includes the JSON Lightning invoice as returned by Lightning
-        Charge's POST /invoice and an authentication token that can be used to modify the order.
-        """
-        data = {"message": self.message,
-                "bid": bid}
-        response = requests.post(url=f"{self.satellite_url}/order", data=data)
-        self.api_status_code = response.status_code
-        if response.status_code == 200:
-            self.place_response = handle_response(response)
-            self.auth_token = self.place_response['auth_token']
-            self.uuid = self.place_response['uuid']
-            return
-        return response.status_code, response.reason, response.text
-
-    def bump_order(self, bid_increase):
-        """Increase the bid for an order sitting in the transmission queue.
-
-        A Lightning invoice is returned for it and, when it is paid, the increase is added to the
-        current bid.
-        """
-        data = {"bid_increase": bid_increase,
-                "auth_token": self.auth_token}
-        response = requests.post(url=f"{self.satellite_url}/order/{self.uuid}/bump", data=data)
-        self.api_status_code = response.status_code
-        if response.status_code == 200:
-            self.bump_response = handle_response(response)
-            return
-        return response.status_code, response.reason, response.text
-
-    def get(self):
-        """Retrieve an order by UUID.
-        """
-        headers = {"X-Auth-Token": self.auth_token}
-        response = requests.get(url=f"{self.satellite_url}/order/{self.uuid}", headers=headers)
-        self.api_status_code = response.status_code
-        if response.status_code == 200:
-            self.get_response = handle_response(response)
-            return
-        return response.status_code, response.reason, response.text
-
-    def delete(self):
-        """To cancel an order.
-        """
-        data = {"auth_token": self.auth_token}
-        response = requests.delete(url=f"{self.satellite_url}/order/{self.uuid}", data=data)
-        self.api_status_code = response.status_code
-        if response.status_code == 200:
-            self.delete_response = handle_response(response)
-            return
-        return response.status_code, response.reason, response.text
+    A Lightning invoice is returned for it and, when it is paid, the increase is added to the
+    current bid.
+    """
+    data = {"bid_increase": bid_increase,
+            "auth_token": auth_token}
+    return requests.post(url=f"{satellite_url}/order/{uuid}/bump", data=data)
 
 
-def pending_orders(sat_url, before_iso8601=None):
+def get(uuid, auth_token, satellite_url):
+    """Retrieve an order by UUID.
+    """
+    headers = {"X-Auth-Token": auth_token}
+    return requests.get(url=f"{satellite_url}/order/{uuid}", headers=headers)
+
+
+def delete(uuid, auth_token, satellite_url):
+    """To cancel an order.
+    """
+    data = {"auth_token": auth_token}
+    return requests.delete(url=f"{satellite_url}/order/{uuid}", data=data)
+
+
+def pending_orders(satellite_url, before_iso8601=None):
     """Retrieve a list of 20 orders awaiting payment ordered by creation time.
 
     For pagination, optionally specify a before parameter (in ISO 8601 format) that specifies
     that the 20 orders immediately prior to the given time be returned.
     """
     if before_iso8601:
-        response = requests.get(url=f"{sat_url}/orders/pending?before={before_iso8601}")
+        return requests.get(url=f"{satellite_url}/orders/pending?before={before_iso8601}")
     else:
-        response = requests.get(url=f"{sat_url}/orders/pending")
-    if response.status_code == 200:
-        return handle_response(response)
-    return response.status_code, response.reason, response.text
+        return requests.get(url=f"{satellite_url}/orders/pending")
 
 
-def queued_orders(sat_url, limit=None):
+def queued_orders(satellite_url, limit=None):
     """Retrieve a list of paid, but unsent orders in descending order of bid-per-byte.
     Both pending orders and the order currently being transmitted are returned.
     Optionally, accepts a parameter specifying how many queued order to return.
     """
     if limit:
-        response = requests.get(url=f"{sat_url}/orders/queued?limit={limit}")
+        return requests.get(url=f"{satellite_url}/orders/queued?limit={limit}")
     else:
-        response = requests.get(url=f"{sat_url}/orders/queued")
-    if response.status_code == 200:
-        return handle_response(response)
-    return response.status_code, response.reason, response.text
+        return requests.get(url=f"{satellite_url}/orders/queued")
 
 
-def sent_orders(sat_url, before_iso8601=None):
+def sent_orders(satellite_url, before_iso8601=None):
     """Retrieves a list of up to 20 sent orders in reverse chronological order.
     For pagination, optionally specify a before parameter (in ISO 8601 format) that specifies
     that the 20 orders immediately prior to the given time be returned.
     """
     if before_iso8601:
-        response = requests.get(url=f"{sat_url}/orders/sent?before={before_iso8601}")
+       return requests.get(url=f"{satellite_url}/orders/sent?before={before_iso8601}")
     else:
-        response = requests.get(url=f"{sat_url}/orders/sent")
-    if response.status_code == 200:
-        return handle_response(response)
-    return response.status_code, response.reason, response.text
+        return requests.get(url=f"{satellite_url}/orders/sent")
 
 
 def sat_ln_node_info(sat_url):
     """Returns information about the c-lightning node where satellite API payments are
     terminated.
     """
-    response = requests.get(url=f"{sat_url}/info")
-    if response.status_code == 200:
-        return handle_response(response)
-    return response
+    return requests.get(url=f"{sat_url}/info")
